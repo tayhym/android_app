c8e6ed637f379445a581160bcea9ab52059f0a23
tayhym <tayhym@gmail.com>
2015-06-24 21:42:30 +0800

    manifest file updated with parent activity, toast messages for when button pressed

--- app/src/main/java/com/example/matthew/myapplication/MainActivity.java
+++ app/src/main/java/com/example/matthew/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -16,6 +17,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // home button as parent up
+
     }
 
 
@@ -26,14 +29,26 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        switch (id) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
         }
-
-        return super.onOptionsItemSelected(item);
     }
+    private void openSearch() {
+        Toast.makeText(this, "search button pressed", Toast.LENGTH_SHORT).show();
+    }
+    private void openSettings() {
+        Toast.makeText(this, "settings button pressed", Toast.LENGTH_SHORT).show();
+    }
+
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
@@ -46,6 +61,8 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu items for use in the action bar
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
+
+
         return super.onCreateOptionsMenu(menu);
     }
 }
